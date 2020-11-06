23d6596eb2eb15ba5f85608401a750b8882b7100
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-11-06 14:40:27 +0200

    testing coveralls

--- code/src/tests/compact.test.js
+++ code/src/tests/compact.test.js
@@ -1,7 +1,7 @@
 import compact from './../compact'
 
 describe("compact", () => {
-  test("large item", () => {
-    expect(compact([0, 1, false, 2, '', 3])).toContain([1, 2, 3])
-  })
+    test("large item", () => {
+        expect(compact([0, 1, false, 2, '', 3])).toContain(2, 3)
+    })
 })
